a0ff061271ca4bc5f3fa5a81bd5bfc1e4dd7a10c
Alex <jalexgould@gmail.com>
2020-09-30 14:23:48 -0400

    Updates

---
+++ precache-manifest.5e760b580e6270a06677cd6317abb0ff.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4003845e015469e2ef33770b481f219a",
+    "url": "/project-five/index.html"
+  },
+  {
+    "revision": "cfe01652b0eae7c61efe",
+    "url": "/project-five/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "2c48aad441ff08786786",
+    "url": "/project-five/static/js/2.cc2c7b27.chunk.js"
+  },
+  {
+    "revision": "70d88436874ad6f0b4ff8dc0e5c19c4e",
+    "url": "/project-five/static/js/2.cc2c7b27.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cfe01652b0eae7c61efe",
+    "url": "/project-five/static/js/main.af13f48d.chunk.js"
+  },
+  {
+    "revision": "f226ed0191f3ab0a77b9",
+    "url": "/project-five/static/js/runtime-main.fd269fbb.js"
+  }
+]);
